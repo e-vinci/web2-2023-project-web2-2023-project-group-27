0af5860397cbb373125ad5f580d344e3b7cb20c7
Steedlan <40958752+Steedlan@users.noreply.github.com>
2023-10-24 10:59:54 +0200

    nickname getter and transition play

--- frontend/src/homepage.js
+++ frontend/src/homepage.js
@@ -1,14 +1,16 @@
 import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
 
-
 const nicknameForm = document.getElementById("nickname");
 const popupSettings = document.getElementById("popupSettings");
 const settingsButton = document.getElementById("options");
+const playForm = document.getElementById("playForm");
+const loadingScreen = document.querySelector(".loadingScreen");
 
 let isPopUpDisplayed = false;
 
 nicknameForm.placeholder = randomNickName();
 popupSettings.style.display = 'none';
+loadingScreen.style.display = 'none';
 
 settingsButton.addEventListener('click', () => {
     if(isPopUpDisplayed) {
@@ -19,6 +21,23 @@ settingsButton.addEventListener('click', () => {
     isPopUpDisplayed = !isPopUpDisplayed;
 })
 
+
+playForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    /* Obtenir le pseudo
+    let nickname;
+    if(nicknameForm.value === "" ||nicknameForm.value === undefined) nickname = nicknameForm.placeholder;
+    else nickname = nicknameForm.value;
+
+     nickname = nickname.replace(/\s/g, "_");
+    */
+
+     // Démarrer l'animation de chargement
+    document.querySelector('.homepage').classList.add('slide-up');
+    document.querySelector('.loadingScreen').classList.add('slide-up');
+    loadingScreen.style.display = 'block';
+})
 
 
 
