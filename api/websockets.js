9b63c8d54e8befb3bc5fdc3405e4fc7c5bbc1d34
Steedlan <40958752+Steedlan@users.noreply.github.com>
2023-10-28 17:41:14 +0200

    test commit

--- api/websockets.js
+++ api/websockets.js
@@ -2,6 +2,7 @@ const WebSocket = require('ws');
 
 const wsserver = new WebSocket.Server({ port: 8082 });
 console.log('WebSocket Server is now running');
+console.log('test commit');
 
 wsserver.on('connection', (ws) => {
   console.log('new client connected');
