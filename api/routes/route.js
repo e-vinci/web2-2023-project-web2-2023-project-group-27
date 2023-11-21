94c1ec48848f7c90ca3463e44dd5c693a075bf45
axel-lefranc-vinci <axel.lefranc@student.vinci.be>
2023-11-21 12:52:10 +0100

    routes #1

--- api/routes/route.js
+++ api/routes/route.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 const User = require('../models/db_conf');
 
-router.post('/add', (req) => {
+router.post('/add', (req, res) => {
   User.save({
     pseudo: req.body.nicknameSignIn,
     email: req.body.emailSignIn,
     password: req.body.passwordSignIn,
   });
-  
+  res.sendStatus(200);
 });
