377cf04818dd697a63350a7980254f24201ba8fe
Steedlan <40958752+Steedlan@users.noreply.github.com>
2023-11-28 09:24:13 +0100

    fix d'un bug qu'on a pas compris

--- frontend/src/script/websockets.js
+++ frontend/src/script/websockets.js
@@ -9,9 +9,9 @@ const { setLoadingBarPercentage, afficherChargement, afficherInformation, stopAf
 const { updatePlayer, removeCard } = require('./game');
 const { generateChatBox, addMessage } = require('./chat');
 
-// const link = 'ws://155.248.239.223:25568';
+const link = 'ws://155.248.239.223:25568';
 
-const link = 'ws://localhost:25568';
+// const link = 'ws://localhost:25568';
 
 let socket;
 let isGameStarted = false;
